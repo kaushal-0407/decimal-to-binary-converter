3b892f04f43c0ac3e05739f5d7ca88e8df399c36
kaushal-0407 <kaushalkumargupta0407@gmail.com>
2026-06-30 17:26:27 +0530

    Added Program

---
+++ DecimalToBinary.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class DecimalToBinary{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A Number To Convert To Binary : ");
+        int dec = sc.nextInt();
+        int quo,rem;
+        quo = dec;
+        String bin = "";
+        do{
+            rem = quo%2;
+            quo/=2;
+            bin+=Integer.toString(rem);
+        } while(quo!=1);
+        bin+="1";
+        StringBuilder sb = new StringBuilder(bin);
+        System.out.println(dec+" in Binary is : "+sb.reverse());
+    }
+}
